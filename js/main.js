db9d4e0261b9e902f117dda4ab5f6c20528ef060
Ivan Golubev <ivan.golubev.spb@gmail.com>
2014-11-11 14:41:06 +0300

    Revert 77aeece..6cd2d9a
    This rolls back to commit 77aeece0069221256a61684b0316424ef3231822.

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ $(function () {
     $("#skype").tooltip({placement : 'bottom', title: "Contact by Skype", container: '#skype'}); 
     
     $('#tlt').textillate({
-        initialDelay: 300,
+        initialDelay: 150,
         in: { effect: 'fadeInLeft', delay: 50 } 
     });
 
