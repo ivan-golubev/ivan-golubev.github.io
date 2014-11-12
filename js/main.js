2b1bbd3d5b118440c11e217bfa2cc81cfa25eccb
Ivan Golubev <ivan.golubev.spb@gmail.com>
2014-11-12 13:49:00 +0300

    resizing
    1. resizing code;
    2. replacing double quotes with single

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    "use strict";  
+    'use strict';  
 
     function onHighResBgLoaded() {
         $('html').addClass('html_high_res'); 
@@ -16,26 +16,26 @@ $(function () {
 	
     function obf0() {
     	var arr = [
-            [103, 114, 111].map(obf4).join(""),	    	
-	    	"eeei",
-			"navi",			
-			[118, 101].map(obf4).join("") + "bulog",				    
+            [103, 114, 111].map(obf4).join(''),	    	
+	    	'eeei',
+			'navi',			
+			[118, 101].map(obf4).join('') + 'bulog',				    
     	];
     	for (var i=0; i<arr.length; i++) {
-    		arr[i] = arr[i].split("").reverse().join("");
+    		arr[i] = arr[i].split('').reverse().join('');
     	}    	
     	return arr;
     }
 
     function obf1(arr) {    	
-    	var cc = ":otlia" + String.fromCharCode(109);
-    	var result = arr.reverse().join(".");
-    	return cc.split("").reverse().join("") + result.replace(".ie", String.fromCharCode(64) + "ie");    	
+    	var cc = ':otlia' + String.fromCharCode(109);
+    	var result = arr.reverse().join('.');
+    	return cc.split('').reverse().join('') + result.replace('.ie', String.fromCharCode(64) + 'ie');    	
     }
 
     function obf2() {
-    	var gg = "tce" + [106, 98].map(obf4).join("")  + "u" + String.fromCharCode(115);
-    	return String.fromCharCode(63) + gg.split("").reverse().join("") + "=Hello%20Ivan";
+    	var gg = 'tce' + [106, 98].map(obf4).join('')  + 'u' + String.fromCharCode(115);
+    	return String.fromCharCode(63) + gg.split('').reverse().join('') + '=Hello%20Ivan';
     }
    
     $('#w42').click( function() {
@@ -47,9 +47,9 @@ $(function () {
     }
 
     function obf3() {
-        return "skype:live:" 
-        + ["spb", [103, 111, 108, 117, 98, 101, 118].map(obf4).join(""), "ivan"].reverse().join(".") 
-        + "?chat";
+        return 'skype:live:' 
+        + ['spb', [103, 111, 108, 117, 98, 101, 118].map(obf4).join(''), 'ivan'].reverse().join('.') 
+        + '?chat';
     }
 
     $('#skype').click( function() {
@@ -57,25 +57,32 @@ $(function () {
     });     
         
     // adding tooltips    
-    $("#cv").tooltip({placement : 'bottom', title: "Open resume", container: '#cv'});
-    $("#w42").tooltip({placement : 'bottom', title: "Contact by email", container: '#w42'});
-    $("#bizcard").tooltip({placement : 'bottom', title: "Get business card", container: '#bizcard'});
-    $("#linkedin").tooltip({placement : 'bottom', title: "Find me on LinkedIn", container: '#linkedin'});    
-    $("#skype").tooltip({placement : 'bottom', title: "Contact by Skype", container: '#skype'}); 
+    $('#cv').tooltip({placement : 'bottom', title: 'Open resume', container: '#cv'});
+    $('#w42').tooltip({placement : 'bottom', title: 'Contact by email', container: '#w42'});
+    $('#bizcard').tooltip({placement : 'bottom', title: 'Get business card', container: '#bizcard'});
+    $('#linkedin').tooltip({placement : 'bottom', title: 'Find me on LinkedIn', container: '#linkedin'});    
+    $('#skype').tooltip({placement : 'bottom', title: 'Contact by Skype', container: '#skype'}); 
 
-    $( window ).resize(function() {
+    function checkSize() {
         var width = $( window ).width();
         var height = $( window ).height();
 
-        if ( width < 500) {
-           // $('logofont').animate({"font-size":"24px"});
-           // $('logofont').hide(); 
-           // $('logofont_small').addClass('logofont_small_phone'); 
+        if ( width < 500) {            
+           $('.logofont').addClass('logofont_small');
+           $('.logofont2').addClass('logofont2_small');
+           $('.main_label').addClass('main_label_small');
+           $('.navbar2').addClass('navbar2_small');
+           $('.obutton').addClass('obutton_small');
         } else {
-           // $('logofont').removeClass('logofont_phone'); 
-           // $('logofont_small').removeClass('logofont_small_phone');  
-        }
-        
-    });    
+           $('.logofont').removeClass('logofont_small');
+           $('.logofont2').removeClass('logofont2_small'); 
+           $('.main_label').removeClass('main_label_small'); 
+           $('.navbar2').removeClass('navbar2_small'); 
+           $('.obutton').removeClass('obutton_small'); 
+        } 
+    }
+
+    checkSize();
+    $( window ).resize(checkSize);    
 
 });
