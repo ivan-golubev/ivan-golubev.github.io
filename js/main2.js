9ff39bc34a42703fec61e704a56d39c3c858388a
Ivan Golubev <ivan.golubev.spb@gmail.com>
2015-03-29 00:41:53 +0300

    storing posts externally

--- js/main2.js
+++ js/main2.js
@@ -2,16 +2,13 @@ $(function () {
 
     'use strict';  
 
-    var view = {
-        img_src:    "img/cat.png",
-        title:      "Cats Everywhere",
-        name_date:  "24 July 2013, Alexander Rechsteiner",
-        body_text:  "Bacon ipsum dolor sit amet esse duis pastrami anim, pancetta fatback capicola officia tenderloin. Meatloaf culpa ut, bacon sed sausage jerky cillum est ham ad laboris ham hock dolore. Venison ut enim, aliqua elit frankfurter et incididunt consequat culpa nostru aliqua elit pancetta. "
-    }; 
+    $.getJSON('articles/1.json',  function(article) {
+            $.get('template.mst', function(template) {
+                var rendered = Mustache.render(template, article);
+                $('#art-1').append(rendered);
+        });
+    }); 
 
-    $.get('template.mst', function(template) {
-        var rendered = Mustache.render(template, view);
-        $('#art-1').append(rendered);
-    });
+
 
 });
