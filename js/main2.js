a971321b5fc23f9fc19c17a7eccb8c1ea7d9d032
Ivan Golubev <ivan.golubev.spb@gmail.com>
2015-03-29 23:36:41 +0300

    pagination
    Adding pagination

--- js/main2.js
+++ js/main2.js
@@ -1,12 +1,19 @@
 
-var page_number = 1, page_size = 4;
+var ivango_net = ivango_net || {};
+
+ivango_net.page_size = 4;
+ivango_net.current_page = 0;
 
 $(function () {
 
     'use strict';  
 
-    function post_articles(articles, template) {
-        var article_pairs = pair(articles);
+    function post_articles(articles, template, page_number) {        
+        var article_pairs = pair(
+                articles.slice(
+                    page_number * ivango_net.page_size,
+                    (page_number + 1) * ivango_net.page_size)
+        );
 
         for (var i = 0; i < article_pairs.length; i++) {
             var $newdiv1 = $("<div class='row'/>");
@@ -14,7 +21,7 @@ $(function () {
             $newdiv1.append(Mustache.render(template, article_pairs[i][0]));
             if (article_pairs[i].length > 1) {
                 $newdiv1.append(Mustache.render(template, article_pairs[i][1]));
-            }
+            }            
             $('#article_container').append($newdiv1);
         }
     }
@@ -47,10 +54,28 @@ $(function () {
     }
 
     $.getJSON('articles/1.json',  function(articles) {
-            $.get('template.mst', function(template) {                
-                post_articles(articles, template);
+            $.get('template.mst', function(articles_template) {  
+                ivango_net.articles = articles;
+                ivango_net.articles_template = articles_template;
+                post_articles(articles, articles_template, ivango_net.current_page);
                 load_tags(articles);
         });
     }); 
+
+    $('#older_posts').click(function() {
+        var total_pages = ivango_net.articles.length / ivango_net.page_size;
+        if (ivango_net.current_page+1 < total_pages) {   
+            $('#article_container').empty();     
+            post_articles(ivango_net.articles, ivango_net.articles_template, ++ivango_net.current_page);
+        }
+    });
+
+    $('#newer_posts').click(function() {
+        var total_pages = ivango_net.articles.length / ivango_net.page_size;
+        if (ivango_net.current_page-1 >= 0) {   
+            $('#article_container').empty();    
+            post_articles(ivango_net.articles, ivango_net.articles_template, --ivango_net.current_page);
+        }
+    });
 
 });
